48f344642f68512563cc98e05661a19b3d2e4556
Chisheng Chen <johnny1001s000602@gmail.com>
2026-04-19 23:17:46 +0800

    Migrate to Linux 7.0

--- generate_patch.sh
+++ generate_patch.sh
@@ -1,21 +1,17 @@
 #!/bin/sh
 
-VERSION=6.19.4
+VERSION=7.0
 BUILDROOT_VERSION=2026.02
 BUILDROOT_LINUX=`pwd`/buildroot-$BUILDROOT_VERSION/output/build/linux-$VERSION
 
 mkdir -p tmp
 cd tmp
 if [ ! -d "linux-$VERSION" ]; then
-    wget "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-$VERSION.tar.xz"
+    wget "https://cdn.kernel.org/pub/linux/kernel/v7.x/linux-$VERSION.tar.xz"
     tar xvf linux-$VERSION.tar.xz
 fi
 
 make -C $BUILDROOT_LINUX ARCH=arm distclean
-
-rm $BUILDROOT_LINUX/.applied_patches_list
-rm $BUILDROOT_LINUX/.br_regen_dot_config
-
 
 
 diff -urN linux-$VERSION \
@@ -24,5 +20,6 @@ diff -urN linux-$VERSION \
     -x ".applied_patches_list" \
     -x ".br_regen_dot_config" \
     -x "*include-prefixes*" \
+    -x "*.files-list.before*" \
     ../buildroot-$BUILDROOT_VERSION/output/build/linux-$VERSION > linux.patch
 # cd ..
