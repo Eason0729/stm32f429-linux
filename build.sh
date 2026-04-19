570250304c5c712667068759c78536781775ba5a
Chisheng Chen <johnny1001s000602@gmail.com>
2026-04-19 21:17:40 +0800

    Rebase buildroot to add missing kernel patches

--- build.sh
+++ build.sh
@@ -8,7 +8,6 @@ BUILDROOT_DIR="$ROOT_DIR/buildroot-$BUILDROOT_VERSION"
 
 patch_buildroot() {
     cp buildroot.config "$BUILDROOT_DIR/configs/stm32f429_disco_xip_defconfig"
-    cp linux.config "$BUILDROOT_DIR/board/stmicroelectronics/stm32f429-disco"
     cp busybox-minimal.config "$BUILDROOT_DIR/package/busybox"
     cp uClibc-ng.config "$BUILDROOT_DIR/package/uclibc"
     cd "$BUILDROOT_DIR"
