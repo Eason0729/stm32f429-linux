25dbf5e99bcfd2169ca12f5547d6abbd7bb80e8e
邱繼叡 Eason CHIU <30045503+Eason0729@users.noreply.github.com>
2026-05-17 13:44:16 +0800

    Use pivot-root to mount mmcblk0 as root filesystem
    Previously, the init program mounted mmcblk0p1 at /root and exec'd
    /root/bin/sh. This rewrites init to properly pivot-root: mount
    proc/sysfs/devtmpfs, mount mmcblk0 (without partition) at /mnt with
    retry on failure, then use MS_MOVE + chroot to switch to the real
    root. On failure, execve logs strerror and pauses.
    
    Update bootargs in corresponding patches: root device changed from
    /dev/mmcblk0p1 to /dev/mmcblk0 to match the init program. The romfs
    initfs patch uses init=/init to invoke this updated init.

--- init/init.c
+++ init/init.c
@@ -1,16 +1,33 @@
 #include <stdio.h>
 #include <sys/mount.h>
-#include <stdlib.h>
 #include <unistd.h>
-
+#include <string.h>
+#include <errno.h>
 
 int main()
 {
-    printf("Mounting rootfs\n");
-    mount("proc", "/proc", "proc", 0, NULL);
-    while (mount("/dev/mmcblk0p1", "/root", "ext2", 0, 0));
+    int i, ret;
 
-    char *argv[] = { "sh", NULL };
-    char *envp[] = {"PATH=/root/bin", NULL};
-    execve("/root/bin/sh", argv, envp);
+    printf("Init starting\n");
+    mount("proc", "/proc", "proc", 0, NULL);
+    mount("sysfs", "/sys", "sysfs", 0, NULL);
+    mount("devtmpfs", "/dev", "devtmpfs", 0, NULL);
+
+    while (mount("/dev/mmcblk0", "/mnt", "ext2", 0, 0)!=0) {
+        usleep(100000);
+        printf("Mounting rootfs fail...\n");
+    }
+
+    chdir("/mnt");
+    mount("/mnt", "/", NULL, MS_MOVE, NULL);
+    chroot(".");
+    chdir("/");
+
+    char *a[] = {"sh", NULL};
+    char *e[] = {"PATH=/bin:/sbin:/usr/bin:/usr/sbin", NULL};
+    execve("/bin/sh", a, e);
+    printf("ERR sh: %s\n", strerror(errno));
+
+    while (1) pause();
+    return 0;
 }
